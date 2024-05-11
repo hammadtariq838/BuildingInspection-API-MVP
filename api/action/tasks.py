39185d5f88a3710e7c81c8d38fd0a49b3902d2c6
Yousuf Khan <24100286@lums.edu.pk>
2024-05-11 05:36:23 +0500

    concrete crack classification basic image added in tasks

--- api/action/tasks.py
+++ api/action/tasks.py
@@ -1,11 +1,22 @@
 # concrete_crack_classification_image, concrete_crack_classification_video, crack_detection_basic_image, crack_detection_basic_video, crack_detection_yolo_v8_image, crack_detection_yolo_v8_video
+import os
+import time
+import cv2
 from celery import shared_task
+from django.core.files import File
+from api.action.ai import crack_detection_basic
 from api.project.asset.models import AssetResult
 
 @shared_task
 def concrete_crack_classification_image(asset_result_id):
   try:
     asset_result = AssetResult.objects.get(id=asset_result_id)
+    image = cv2.imread(asset_result.asset.file.path)
+    
+
+
+
+
   except Exception as e:
     asset_result.error = str(e)
     asset_result.status = 'failed'
@@ -26,6 +37,29 @@ def concrete_crack_classification_video(asset_result_id):
 def crack_detection_basic_image(asset_result_id):
   try:
     asset_result = AssetResult.objects.get(id=asset_result_id)
+
+    # read the image
+    image = cv2.imread(asset_result.asset.file.path)
+    result, ratio = crack_detection_basic(image)
+
+    image_path = asset_result.asset.file.path
+    image_name = asset_result.asset.file.name
+
+    temp_image_path = image_path.replace('assets', 'temp')
+    cv2.imwrite(temp_image_path, result)
+    result_image_name = image_name.replace('assets/', '')
+    # attact time to the result image name
+    result_image_name = f'{str(int(time.time()))}_{result_image_name}'
+    asset_result.result.save(result_image_name, File(open(temp_image_path, 'rb')))
+    os.remove(temp_image_path)
+
+    """
+      @Yousuf24100286
+      Save the metadata to the database
+    """
+
+    asset_result.status = 'completed'
+    asset_result.save()
   except Exception as e:
     asset_result.error = str(e)
     asset_result.status = 'failed'
@@ -46,6 +80,9 @@ def crack_detection_basic_video(asset_result_id):
 def crack_detection_yolo_v8_image(asset_result_id):
   try:
     asset_result = AssetResult.objects.get(id=asset_result_id)
+    image = cv2.imread(asset_result.asset.file.path)
+
+    
   except Exception as e:
     asset_result.error = str(e)
     asset_result.status = 'failed'
